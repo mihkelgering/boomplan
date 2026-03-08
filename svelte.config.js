e349b14507f789540714a54c524a37af46ed0c8f
Mihkel Gering <mihkel.gering@gmail.com>
2026-03-08 13:51:24 +0200

    update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      fallback: '404.html'
-    }),
-    prerender: {
-      entries: ['*']
-    },
-    paths: {
-      base: '/boomplan'
+    kit: {
+        adapter: adapter({
+            fallback: '404.html'
+        }),
+        prerender: {
+            entries: ['*']
+        },
+        paths: {
+            base: '/boomplan'
+        },
+        trailingSlash: 'always'
     }
-  }
 };
 
 export default config;
