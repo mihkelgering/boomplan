2fac7632ce253451ad60bb7360565977df126eba
Mihkel Gering <mihkel.gering@gmail.com>
2026-03-08 13:14:00 +0200

    Add cannonball and update json files

--- scripts/download-icons.js
+++ scripts/download-icons.js
@@ -11,7 +11,7 @@ const ICONS = {
   rocket:            'ammo.rocket.basic_512.png',
   satchel:           'explosive.satchel_512.png',
   explosive_ammo:    'ammo.rifle.explosive_512.png',
-  cannon:            'cannon_512.png',
+  cannon:            'https://wiki.rustclash.com/img/items180/cannonball.png',
   incendiary_rocket: 'ammo.rocket.fire_512.png',
   fire_arrow:        'arrow.fire_512.png',
   chainsaw:          'chainsaw_512.png',
@@ -19,6 +19,20 @@ const ICONS = {
   hatchet:           'hatchet_512.png',
   stone_hatchet:     'stonehatchet_512.png',
   pickaxe:           'pickaxe_512.png',
+  // New melee weapons
+  rock:              'rock_512.png',
+  bone_club:         'bone.club_512.png',
+  bone_knife:        'knife.bone_512.png',
+  hunting_knife:     'knife.butcher_512.png',
+  combat_knife:      'knife.combat_512.png',
+  wooden_spear:      'spear.wooden_512.png',
+  stone_spear:       'spear.stone_512.png',
+  machete:           'machete_512.png',
+  mace:              'mace_512.png',
+  salvaged_cleaver:  'salvaged.cleaver_512.png',
+  salvaged_sword:    'salvaged.sword_512.png',
+  longsword:         'longsword_512.png',
+  salvaged_hammer:   'hammer.salvaged_512.png',
 };
 
 const outDir = path.resolve('static/weapons');
@@ -28,8 +42,13 @@ const results = [];
 
 function download(id, file) {
   return new Promise((resolve) => {
-    const url = BASE + file;
+    const url = file.startsWith('http') ? file : BASE + file;
     const dest = path.join(outDir, `${id}.png`);
+    if (fs.existsSync(dest)) {
+      results.push(`SKIP ${id}: already exists`);
+      resolve();
+      return;
+    }
     https.get(url, (res) => {
       if (res.statusCode !== 200) {
         res.resume();
